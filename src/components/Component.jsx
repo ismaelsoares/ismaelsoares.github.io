5e1a873ae82ca5a9540073481d4fc47b5fa6e065
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2023-09-16 22:31:37 -0300

    Add component image and h1

---
+++ src/components/Component.jsx
@@ -0,0 +1,7 @@
+export const Component = () => {
+    return (
+        <div>
+            <h1>Esta é minha página inicial com React</h1>
+        </div>
+    )
+}
---
+++ src/components/Image.jsx
@@ -0,0 +1,5 @@
+export const Image = () => {
+    return (
+        <img src="https://avatars.githubusercontent.com/u/5531526?s=400&u=f1e904b622e32751d85bbbd8a1d76b5a2c0b5271&v=4" alt="avatar" />
+    )
+}
